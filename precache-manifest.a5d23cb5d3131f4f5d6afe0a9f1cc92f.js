4a3c0e4a60074d0b2d123adb546526941b930888
Yashoo Anarajula <yashoo5571@gmail.com>
2020-08-08 00:32:59 -0500

    Updates

---
+++ precache-manifest.a5d23cb5d3131f4f5d6afe0a9f1cc92f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26fc2db0b2071b4fc7c768518cfcbdbf",
+    "url": "/projj-that-works/index.html"
+  },
+  {
+    "revision": "38603723334f5dc72ab9",
+    "url": "/projj-that-works/static/css/main.db1a5f1a.chunk.css"
+  },
+  {
+    "revision": "d90cd5d8f7673bb0f3b8",
+    "url": "/projj-that-works/static/js/2.63b38059.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projj-that-works/static/js/2.63b38059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38603723334f5dc72ab9",
+    "url": "/projj-that-works/static/js/main.bd4f657e.chunk.js"
+  },
+  {
+    "revision": "8320a8aaf41d5b5c8289",
+    "url": "/projj-that-works/static/js/runtime-main.5e91e7c3.js"
+  },
+  {
+    "revision": "5665854d266d9512ca72b4206160a8f7",
+    "url": "/projj-that-works/static/media/Pics Cart.5665854d.png"
+  },
+  {
+    "revision": "c8b4d1dee63a4c76550f7da8f399da9f",
+    "url": "/projj-that-works/static/media/chck.c8b4d1de.jpg"
+  },
+  {
+    "revision": "0f0eed68bff694c1f5bdf44d78c9c8dd",
+    "url": "/projj-that-works/static/media/cicd.0f0eed68.png"
+  },
+  {
+    "revision": "b743c24b23c5c75e7d6e28d4041fe8f8",
+    "url": "/projj-that-works/static/media/crud.b743c24b.png"
+  },
+  {
+    "revision": "90d2eb733ed78208d21f208ba859acdf",
+    "url": "/projj-that-works/static/media/dribbble.90d2eb73.png"
+  },
+  {
+    "revision": "3399ec8413e6f20178a817f2bd950e1d",
+    "url": "/projj-that-works/static/media/leda off.3399ec84.png"
+  },
+  {
+    "revision": "47afe9fb0f753904df05a52d5c1b50c5",
+    "url": "/projj-that-works/static/media/mypicmypic.47afe9fb.jpg"
+  },
+  {
+    "revision": "2cf59b3bac0e82ad83661fe91edcac68",
+    "url": "/projj-that-works/static/media/polly archi.2cf59b3b.jpg"
+  },
+  {
+    "revision": "74a1c6dbaa76eec1efcc3f63568aedb8",
+    "url": "/projj-that-works/static/media/sane.74a1c6db.png"
+  },
+  {
+    "revision": "8969e1b9da08954c35a31d056b70a766",
+    "url": "/projj-that-works/static/media/sbot.8969e1b9.png"
+  },
+  {
+    "revision": "4676b89b0fe1a69b61a4f5770ea4de77",
+    "url": "/projj-that-works/static/media/styper.4676b89b.png"
+  }
+]);
